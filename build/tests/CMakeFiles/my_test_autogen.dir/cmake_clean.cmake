de732e2dca96085dce526effe4fef726c4ce7edd
GrabCrabD <105257214+GrabCrabD@users.noreply.github.com>
2023-09-04 17:11:39 +0400

    done

--- build/tests/CMakeFiles/my_test_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/my_test_autogen"
-  "my_test_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/my_test_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/tests/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/dmitriy/repository/for git/RS/toDoApp/src/tests
-# Build directory: /Users/dmitriy/repository/for git/RS/toDoApp/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/dmitriy/repository/for git/RS/toDoApp/build/tests/my_test[1]_include.cmake")
-add_test(my_test "/Users/dmitriy/repository/for git/RS/toDoApp/build/tests/my_test")
-set_tests_properties(my_test PROPERTIES  _BACKTRACE_TRIPLES "/Users/dmitriy/repository/for git/RS/toDoApp/src/tests/CMakeLists.txt;14;add_test;/Users/dmitriy/repository/for git/RS/toDoApp/src/tests/CMakeLists.txt;0;")
